47a6cde3160da2bc1696ffc28d79ff471698e0d7
Chorn Sokun <csokun@users.noreply.github.com>
2024-05-02 12:51:26 +1000

    Update pkgs (#4)
    * remove gulp
    * update npm packages

--- test/integration/streaming_test.js
+++ test/integration/streaming_test.js
@@ -2,13 +2,13 @@ const http = require("http");
 const url = require("url");
 const port = 3100;
 
-describe("integration tests: streaming", function(done) {
+describe("integration tests: streaming", function (done) {
   if (typeof window !== "undefined") {
     done();
     return;
   }
 
-  it("handles onerror", function(done) {
+  it("handles onerror", function (done) {
     let server;
     let closeStream;
 
@@ -40,21 +40,27 @@ describe("integration tests: streaming", function(done) {
     });
   });
 
-  it("handles close message", function(done) {
+  it("handles close message", function (done) {
     let server;
     let closeStream;
 
     const requestHandler = (request, response) => {
-      request.on("close", (e) => {
+      // the streamer will retry indefinitely (by design), so we want to ensure
+      // that the test is only completed once
+      if (!server.listening) {
+        return;
+      }
+
+      request.once("close", (e) => {
         closeStream();
         server.close();
         done();
       });
 
       response.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
       });
       response.write("retry: 10\nevent: close\ndata: byebye\n\n");
     };
@@ -72,12 +78,54 @@ describe("integration tests: streaming", function(done) {
         .operations()
         .stream({
           onmessage: (m) => {
-            done("unexpected message "+JSON.stringify(m));
+            done("unexpected message " + JSON.stringify(m));
           },
           onerror: (err) => {
             done(err);
           },
         });
     });
+  });
+});
+
+describe("end-to-end tests: real streaming", function (done) {
+  if (typeof window !== "undefined") {
+    done();
+    return;
+  }
+
+  // stream transactions from pubnet for a while and ensure that we cross a
+  // ledger boundary (if streaming is broken, we will get stuck on a single
+  // ledger's transaction batch).
+  it("streams in perpetuity", function (done) {
+    const DURATION = 30;
+    const server = new StellarSdk.Server("https://horizon.stellar.org");
+    this.timeout((DURATION + 5) * 1000); // pad timeout
+
+    let transactions = [];
+
+    const finishTest = (err) => {
+      clearTimeout(timeout);
+      closeHandler();
+
+      expect(transactions.length).to.be.gt(0);
+      let firstLedger = transactions[0].ledger_attr;
+      let lastLedger = transactions[transactions.length - 1].ledger_attr;
+      expect(lastLedger - firstLedger).to.be.gte(1);
+
+      done(err);
+    };
+
+    let closeHandler = server
+      .transactions()
+      .cursor("now")
+      .stream({
+        onmessage: (msg) => {
+          transactions.push(msg);
+        },
+        onerror: finishTest,
+      });
+
+    let timeout = setTimeout(finishTest, DURATION * 1000);
   });
 });
